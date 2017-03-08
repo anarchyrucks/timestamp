ba5066d0443c5244eb49c7837184b8d4f3665c91
Ashish Acharya <anarchyrucks@live.com>
2017-03-08 22:09:47 +0530

    Initial Commit

---
+++ server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Time struct {
+	Unix    int64  `json:"unix"`
+	Natural string `json:"natural"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", RootHandler)
+	r.HandleFunc("/{time}", TimeHandler)
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./client/index.html")
+}
+
+func TimeHandler(w http.ResponseWriter, r *http.Request) {
+	str := mux.Vars(r)["time"]
+	w.WriteHeader(http.StatusOK)
+	if timestamp, err := strconv.Atoi(str); err == nil {
+		t := time.Unix(int64(timestamp), 0)
+		timestring := t.Format("January 02, 2006")
+		json.NewEncoder(w).Encode(Time{
+			int64(timestamp),
+			timestring,
+		})
+	} else {
+		t, err := time.Parse("January 02, 2006", str)
+		if err != nil {
+			fmt.Fprintf(w, "Invalid input")
+			os.Exit(0)
+		}
+		timestamp := t.Unix()
+		timestring := str
+		json.NewEncoder(w).Encode(Time{
+			timestamp,
+			timestring,
+		})
+	}
+}
